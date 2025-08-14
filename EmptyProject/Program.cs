bf6b875ef84422c3bd5ce11765db7f11bfd54eef
Вадим Зинович <redotv1@gmail.com>
2025-08-14 23:52:09 +0300

    solve 4 leetcode tasks

--- EmptyProject/Program.cs
+++ EmptyProject/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿public class Solution {
+    public int[] SingleNumber(int[] nums) {
+        return nums.GroupBy((k) => k).ToDictionary(gr => gr.Key, gr => gr.Count()).Where(gr => gr.Value == 1).Select(gr => gr.Key).ToArray();      
+    }
+}
